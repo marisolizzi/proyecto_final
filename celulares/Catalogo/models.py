a917d5e262c14caa31ab5831725d8fffbbeb2827
Marisol Izzi <marisol.izzi@gmail.com>
2024-01-24 21:45:04 -0300

    Definición de Modelos

--- celulares/Catalogo/models.py
+++ celulares/Catalogo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+    detalle = models.CharField(max_length=1000)
+    imagen = models.CharField(max_length=100)
+
+class Celular(models.Model):
+    nombre = models.CharField(max_length=50)
+    detalle = models.CharField(max_length=1000)
+    categoria = models.IntegerField()
+    imagen = models.CharField(max_length=100)
